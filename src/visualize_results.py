87006c9c8a7bf2ff21557ad180b224771255ce69
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2025-01-21 18:42:37 +0100

    improved visualization

--- src/visualize_results.py
+++ src/visualize_results.py
@@ -1,3 +1,4 @@
+import importlib
 import pickle
 import sys
 
@@ -9,9 +10,13 @@ PROBLEM = sys.argv[1]
 
 x = np.load(f"data/problem_{PROBLEM}.npz")["x"]
 y = np.load(f"data/problem_{PROBLEM}.npz")["y"]
-with open(f"results/problem_{PROBLEM}", "rb") as fs:
-    f = pickle.load(fs)
+# with open(f"results/problem_{PROBLEM}", "rb") as fs:
+#     f = pickle.load(fs)
+mod = importlib.import_module(f"s327797")
+f = mod.__getattribute__(f"f{PROBLEM}")
 
 # f = lambda x: x[0] + np.sin(x[1]) - 0.8 * np.sin(x[1])
-print(f"MSE: {np.mean((y - f(x)) ** 2)}")
+print(f"MSE: {np.mean((y - f(x)) ** 2):.3e}")
+nrmse = np.sqrt(np.mean((y - f(x)) ** 2)) / (y.max() - y.min())
+print(f"NRMSE: {nrmse:.3e}")
 visualize_result(x, y, f, block=True)
